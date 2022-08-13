c6b88dc2fa0c5e1480d60e837500b7ad85aad73f
fatemehGH78 <108860264+fatemehGH78@users.noreply.github.com>
2022-08-13 08:49:56 -0700

    Add files via upload

---
+++ log in/js/min2.js
@@ -0,0 +1,13 @@
+function validate(){
+    var username=document.getElementById('email').value;
+    var password=document.getElementById('pass').value;
+
+if(password.indexOf(' ')!=-1){
+    alert("فضای خالی در رمز نباید وجود داشته باشد ...")
+
+}
+ 
+
+
+}
+
